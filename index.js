f14f84d9ac836400342110dd73dbc6744f215a6d
Giwon Lim <giwon.dev@gmail.com>
2022-01-06 23:14:49 +0900

    사용자 조회 api 테스트 코드 작성

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ var users = [
 app.use(morgan('dev'));
 
 app.get('/users', (req, res) => {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  const user = users.find((user) => user.id === id);
+  if (!user) return res.status(404).end();
+  res.json(user);
 });
 
 app.listen(3000, console.log('Example app listening on port 3000!'));
