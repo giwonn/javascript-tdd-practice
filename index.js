ce6a8b2f48ed0fb5d54e10f06fc03ce6241d3b33
Giwon Lim <giwon.dev@gmail.com>
2022-01-08 23:37:58 +0900

    api CRUD 테스트 코드 작성

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var users = [
 ];
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/users', (req, res) => {
   req.query.limit = req.query.limit || 10;
@@ -24,6 +26,41 @@ app.get('/users/:id', (req, res) => {
   const user = users.find((user) => user.id === id);
   if (!user) return res.status(404).end();
   res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  users = users.filter((user) => user.id !== id);
+  res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).end();
+  if (users.some((user) => user.name === name)) return res.status(409).end();
+  const id = Date.now();
+  const user = { id, name };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.some((user) => user.name === name);
+  if (isConflict) return res.status(409).end();
+
+  const user = users.find((user) => user.id === id);
+  if (!user) return res.status(404).end();
+
+  user.name = name;
+
+  return res.json(user);
 });
 
 app.listen(3000, console.log('Example app listening on port 3000!'));
